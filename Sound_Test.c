48b47c36826adfc6d2157da430b7bc50fc87400f
gvachonwe <102626427+gvachonwe@users.noreply.github.com>
2022-09-07 17:44:56 -0400

    v2

--- Sound_Test.c
+++ Sound_Test.c
@@ -5,15 +5,13 @@ task main()
 	setSoundVolume(10);
 
 	playSoundFile("Magic wand.rsf");
-  wait1Msec(2000);
+  	wait1Msec(2000);
 
-  setSoundVolume(20);
+  	setSoundVolume(20);
 
-  playSoundFile("Good job.rsf");
-  wait1Msec(1000);
+  	playSoundFile("Good job.rsf");
+  	wait1Msec(1000);
 
 	//playSound(beep);
 	//playTone(40, 100);
-
-
 }
